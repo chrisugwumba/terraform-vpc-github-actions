e65c5b1d254dd1a4e25b5135e1fcd4d85339d9e8
Chrisangelo <chrisangelo28@gmail.com>
2026-07-14 19:12:06 +0100

    Updated the security group and the associated ingress and egress rules

---
+++ modules/sg/main.tf
@@ -0,0 +1,38 @@
+resource "aws_security_group" "sg" {
+  name        = "security_group"
+  description = "Allow HTTP and SHH inbound traffic and all outbound traffic"
+  vpc_id      = var.vpc_id
+
+  tags = {
+    Name = "MySecurityGroup"
+  }
+}
+
+# 2. INBOUND RULE: HTTP (Port 80) from the World
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.sg.id
+  cidr_ipv4         = "0.0.0.0/0" # Open to the public internet
+  from_port         = 80
+  to_port           = 80
+  ip_protocol       = "tcp"
+  description       = "Allows all traffic access to your vpc" # <-- Use this instead of tags
+}
+
+# 3. INBOUND RULE: SSH (Port 22) from a Restricted IP
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.sg.id
+  cidr_ipv4         = "0.0.0.0/0" # SEC_TIP: Swap with your actual home/work public IP
+  from_port         = 22
+  to_port           = 22
+  ip_protocol       = "tcp"
+  description       = "Allow SSH access" # <-- Use this instead of tags
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"                      # semantically equivalent to all ports
+  description       = "Public HTTP web traffic" # <-- Use this instead of tags
+}
+
+
